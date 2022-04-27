b23cc4a8b1e402e576d8e1327e55ac64e6ad3dcb
fed.emran <fed.emran@gmail.com>
2022-04-27 10:20:08 +0600

    Mobile tab

--- src/app/pages/project-detail/project-tab/project-tab.component.ts
+++ src/app/pages/project-detail/project-tab/project-tab.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectTabComponent implements OnInit {
 
+  showDetail: boolean;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  tabChange(){
+    this.showDetail = !this.showDetail
+  }
 }
