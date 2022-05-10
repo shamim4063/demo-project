8a67b0442b774d308987024045cab19c037d67d1
shamim4063 <shamim.vc@gmail.com>
2022-05-10 14:37:55 +0600

    updated

--- src/app/pages/project-detail/sales-tab/sales-tab.service.ts
+++ src/app/pages/project-detail/sales-tab/sales-tab.service.ts
@@ -1,7 +1,5 @@
-import { HttpParams } from '@angular/common/http';
 import { Injectable, Injector } from '@angular/core';
 import { BaseService } from 'src/app/shared/services/base.service';
-import { SalesTabModule } from './sales-tab.module';
 
 @Injectable()
 export class SalesTabService extends BaseService{
@@ -9,7 +7,5 @@ export class SalesTabService extends BaseService{
   constructor(injector: Injector) { 
     super(injector);
   }
-
-  
 
 }
